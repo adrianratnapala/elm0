5149d9ae2bcd2108a5d1e56a841e906874d67788
Adrian Ratnapala <adrian.ratnapala@gmail.com>
2011-11-24 20:17:09 +0100

    0unit errors now have file:lineno:func format.
    The FAIL patern in n0run.py has been updated accordingly.  I suppose this
    counts as a hack, because I probably once thought n0run was going to
    become more generic, not less.
    
    This will hopefully help Vim quickfix find my errors.

--- 0unit.h
+++ 0unit.h
@@ -1,26 +1,35 @@
 #ifndef _0UNIT_H
 #define _0UNIT_H
 
+// FIX: CHK should goto fail;
+
 #include <stdio.h>
 #define CHK(T) do {\
-                        if( !chk( !!(T), __func__, #T) ) return 0; \
+                        if( !chk( !!(T), __FILE__, __LINE__, __func__, #T) ) \
+                                return 0; \
                } while(0)
 
-#define WRN(T) wrn( !!(T), __func__, #T) 
+#define WRN(T) wrn( !!(T), __FILE__, __LINE__, __func__, #T)
 
 #define PASS() return pass( __func__ )
 
-inline static int chk(int pass, const char *test, const char *text) {
-        if( pass ) 
+inline static int chk(int pass, const char *file, int line,
+                                const char *test, const char *text)
+{
+        if( pass )
                 return 1;
-        printf("\033[31m\033[1mFAILED:\033[0m %s <%s>\n", test, text);
+        printf("\033[31m\033[1mFAILED:\033[0m %s:%d:%s <%s>\n",
+                file, line, test, text);
         return 0;
 }
 
-inline static int wrn(int pass, const char *test, const char *text) {
-        if( pass ) 
+inline static int wrn(int pass, const char *file, int line,
+                                const char *test, const char *text)
+{
+        if( pass )
                 return 1;
-        printf("\033[34m\033[1mWARNING:\033[0m %s <%s>\n", test, text);
+        printf("\033[34m\033[1mWARNING:\033[0m %s:%d:%s <%s>\n",
+                file, line,test, text);
         return 0;
 }
 
@@ -30,4 +39,3 @@ inline static int pass(const char *test) {
 }
 
 #endif /* _0UNIT_H */
-
