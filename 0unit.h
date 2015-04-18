87eb75363b1e9f7d30d05326c6e642edded890e1
Adrian Ratnapala <raka@gollum>
2015-04-18 08:27:35 +0200

    0unit: CHKV, CHK with a free-form format string.
    This version does not print the test that failed, instead it prints your format
    string.  Probably most useful when wrapped by a helper macro.

--- 0unit.h
+++ 0unit.h
@@ -21,16 +21,13 @@
 # endif
 #endif
 
-#define CHK(T) do {\
-                        if(!chk(!!(T), __FILE__, __LINE__, __func__, "%s", #T))\
+#define CHKV(T, ...) do {\
+                        if(!chk(!!(T), __FILE__, __LINE__, __func__, __VA_ARGS__))\
                                 goto fail; \
                } while(0)
 
-#define FAIL(...) do {\
-                        if(!chk(0, __FILE__, __LINE__, __func__, \
-                                   __VA_ARGS__)) goto fail;      \
-               } while(0)
-
+#define CHK(T) CHKV(T, "%s", #T)
+#define FAIL(...) CHKV(0, __VA_ARGS__)
 #define WRN(...) wrn( 0, __FILE__, __LINE__, __func__, __VA_ARGS__)
 
 
